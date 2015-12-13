59e0e2134b9d50c82f31c6a2ede29072f7f35586
KoenvE <koen_van_emden@hotmail.com>
2015-12-13 22:11:26 +0100

    Client added
    - Added client start
    - Added frequency visualizer
    - Started with timer instances
    - Kick detection (head grows on kick)
    - Convert request in node now prints out a tracklist.txt in the tracks
    folder
    - Client reads tracklist.txt to randomly choose track (testing purposes)

--- client/src/js/client.js
+++ client/src/js/client.js
@@ -1,3 +1,164 @@
 (function() {
-    //Client JS
+
+    var test = {};
+    test.track = '1.Hardstyle637Mark van der Pijl.mp3'; //Change to test title in track folder
+    test.dir = '../client/tracks/';
+    test.src = test.dir + test.track;
+
+    $(window).resize(function() {
+        ui.resize();
+    });
+
+    var w = {};
+    w.w = function() {
+        return $(window).width();
+    };
+    w.h = function() {
+        return $(window).height();
+    };
+
+    var tool = {};
+    tool.readFile = function(file, callback) {
+        var rawFile = new XMLHttpRequest();
+        rawFile.open("GET", file, false);
+        rawFile.onreadystatechange = function() {
+            if (rawFile.readyState === 4) {
+                if (rawFile.status === 200 || rawFile.status === 0) {
+                    var content = rawFile.responseText;
+                    console.log(content);
+                    if(callback) callback(content);
+                }
+            }
+        };
+        rawFile.send(null);
+    };
+
+    var color = {};
+    color.bg = '#1e2230';
+    color.blue = '#26477d';
+
+    var elements = {};
+    elements.theater = $('#theater');
+    elements.skull = $('#skull');
+
+    var audio = {};
+    audio.audio = new Audio();
+    audio.dancer = new Dancer();
+    audio.setSrc = function(url) {
+        audio.dancer.load({
+            'src': url
+        });
+    };
+    audio.startPlaylist = function(trackListArray){
+        var src = trackListArray[Math.floor(Math.random()*trackListArray.length)];
+        audio.setSrc(test.dir + src);
+        audio.play();
+    };
+    audio.play = function() {
+        audio.dancer.play();
+    };
+    audio.pause = function() {
+        audio.dancer.pause();
+    };
+    audio.setVolume = function(vol) {
+        audio.dancer.setVolume(vol); //Volume from 0 to 1
+    };
+    audio.getSpectrum = function() {
+        return audio.dancer.getSpectrum();
+    };
+    audio.getWaveform = function() {
+        return audio.dancer.getWaveform();
+    };
+    audio.kick = audio.dancer.createKick({
+        frequency: [0, 10],
+        threshold: 0.5,
+        onKick: function(mag) {
+            ui.kick();
+        },
+        offKick: function(mag) {
+            ui.noKick();
+        }
+    });
+    audio.kick.on();
+
+    var ui = {};
+    ui.c = document.getElementById("canvas");
+    ui.ctx = ui.c.getContext("2d");
+    ui.resize = function() {
+        ui.c.width = w.w();
+        ui.c.height = w.h();
+    };
+    ui.render = function() {
+        requestAnimationFrame(ui.render);
+
+        var curMs = timing.getCurMs();
+
+        ui.draw();
+    };
+    ui.kick = function() {
+        console.log('kick');
+        TweenLite.to(elements.theater, 0.1, {
+            scale: 1.1
+        });
+    };
+    ui.noKick = function() {
+        TweenLite.to(elements.theater, 0.4, {
+            scale: 1
+        });
+    };
+    ui.draw = function() {
+        var spectrum = {};
+        spectrum.data = audio.dancer.getSpectrum();
+        spectrum.size = spectrum.data.length / 2;
+        spectrum.barWidth = (w.w() / spectrum.size) * 2.5;
+        x = 0;
+
+        ui.ctx.fillStyle = color.bg;
+        ui.ctx.fillRect(0, 0, w.w(), w.h());
+        for (var i = 0; i < spectrum.size; i++) {
+            spectrum.barHeight = spectrum.data[i] * 1500;
+            ui.ctx.fillStyle = color.blue;
+            ui.ctx.fillRect(x, w.h() / 2 - spectrum.barHeight / 2, spectrum.barWidth, spectrum.barHeight);
+            x += spectrum.barWidth;
+        }
+    };
+
+    //Starting it all
+    tool.readFile(test.dir+'tracklist.txt', function(data){
+        data = data.split(',');
+        console.log(data);
+        audio.startPlaylist(data);
+        ui.render();
+    });
+    ui.resize();
+
+    var timing = {};
+    timing.deadline = '2015-12-31';
+    timing.getRemaining = function() {
+        var t = Date.parse(timing.deadline) - Date.now();
+        var s = Math.floor((t / 1000) % 60);
+        var m = Math.floor((t / 1000 / 60) % 60);
+        var h = Math.floor((t / (1000 * 60 * 60)) % 24);
+        var d = Math.floor(t / (1000 * 60 * 60 * 24));
+        return {
+            'total': t,
+            'days': d,
+            'hours': h,
+            'minutes': m,
+            'seconds': s
+        };
+    };
+    timing.getCurMs = function() {
+        return Date.now();
+    };
+    timing.getRemainingMs = function() {
+        return timing.getRemaining().total;
+    };
+    console.log('CurMs:' + timing.getCurMs());
+    console.log('Till deadline:');
+    var rem = timing.getRemaining();
+    console.log('Days:' + rem.days);
+    console.log('hours:' + rem.hours);
+    console.log('minutes:' + rem.minutes);
+    console.log('seconds:' + rem.seconds);
 })();
--- node_custom/media.js
+++ node_custom/media.js
@@ -1,11 +1,13 @@
-var fs = require('fs'),
-    ytdl = require('ytdl-core'),
-    ffmpeg = require('fluent-ffmpeg'),
-    spread = require('google-spreadsheet'),
+var fs = require('fs'), //Filesystem
     path = require('path'),
-    sheet = new spread('1yHVy27G84f53ejmfYQ_xT9ibjLwdTscPmpk2pPJ-YtI');
+    ytdl = require('ytdl-core'), //Youtube flv downloader
+    ffmpeg = require('fluent-ffmpeg'), //Convert utility
+    spread = require('google-spreadsheet'),
+    sheet = new spread('1HqcTW5rwxhe8a7d398tE0Q_Dmp2O2ijF8I4_Sm6i5c4'), //Spreadsheet ID
+    trackFolder = './client/tracks'; //Tracks now reside in client folder where they are needed
 
 var media = function() {
+
 
     var error = {};
     error.err = function(err, callback) {
@@ -14,43 +16,49 @@ var media = function() {
         });
     };
 
+
     var tool = {};
-    tool.getYTID = function(url) { //Get Id from any format of youtube url.
+    //Get Id from any format of youtube url.
+    tool.getYTID = function(url) {
         var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
         var match = url.match(regExp);
         return (match && match[7].length == 11) ? match[7] : false;
     };
-    tool.YTNormalize = function(url){ //Normalize any format to the generic video url.
+    //Normalize any format to the generic video url. (Built because mobile urls caused problems)
+    tool.YTNormalize = function(url) {
         var id = tool.getYTID(url);
-        return 'https://www.youtube.com/watch?v='+id;
+        return 'https://www.youtube.com/watch?v=' + id;
     };
 
+
+    //Download FLV and convert to mp3
     this.convert = function(url, name, genre, callback) {
         var nameString = genre + Math.floor(Math.random() * 1000).toString() + name;
-        var flv = path.resolve(__dirname, nameString + '.flv');
+        var flv = path.resolve(trackFolder, nameString + '.flv');
         ytdl(tool.YTNormalize(url))
             .pipe(fs.createWriteStream(flv))
             .on('finish', function() {
                 ffmpeg()
                     .input(flv)
                     .audioCodec('libmp3lame')
-                    .save(path.resolve(__dirname, nameString + '.mp3'))
+                    .save(path.resolve(trackFolder, nameString + '.mp3'))
                     .on('error', console.error)
                     .on('end', function() {
-                        fs.unlink(flv)
                         console.log('Finished: ' + nameString);
-                        if (callback) callback();
+                        fs.unlink(flv)
+                        if (callback) callback(nameString+'.mp3');
                     });
             });
     };
-
+    //Get sheetdata from google
     this.getSheet = function(callback) {
         sheet.getRows(1, function(err, data) {
             if (err) return connection.err(err, callback);
-            console.log(data);
             if (callback) callback(data);
+            console.log(data);
         });
     };
+
 
 };
 module.exports = media;
