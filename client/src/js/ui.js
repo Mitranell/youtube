ae225563f1c9db6026413422a0ce81cb43a6bf56
Mitranell <milantheeuwes@live.nl>
2015-12-20 21:57:13 +0100

    Bar height
    Dynamic bar height depending on canvasheight

--- client/src/js/ui.js
+++ client/src/js/ui.js
@@ -23,8 +23,7 @@ var ui = function(dom) {
         c.ctx.clearRect(0, 0, dom.canvasWrapperWidth(), dom.canvasWrapperHeight());
         for (var i = 0; i < spectrum.data.length; i++) {
             if (i < spectrum.size) {
-              //TODO We have to choose a value which is best for the TV
-              spectrum.barHeight = spectrum.data[i] * 1300;
+              spectrum.barHeight = spectrum.data[i] * dom.canvasWrapperHeight();
               c.ctx.fillStyle = color.white;
               c.ctx.fillRect(x, dom.canvasWrapperHeight() / 2 - spectrum.barHeight / 2, spectrum.barWidth, spectrum.barHeight);
               x += spectrum.barWidth * 2; //Makes it display 1/12th of the spectrum
@@ -32,7 +31,7 @@ var ui = function(dom) {
 
             if (spectrum.data[i] > max) max = spectrum.data[i];
         }
-        
+
         //TODO choose between these two lines (or variants of it):
         //dom.kick((spectrum.data[1] + spectrum.data[2]) / 4);
         dom.kick(max / 2);
