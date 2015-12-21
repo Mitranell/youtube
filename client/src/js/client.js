e72d873cc301254e2adcf6e674e7dadd9cb94053
Mitranell <milantheeuwes@live.nl>
2015-12-22 00:16:03 +0100

    Loop through whole playlist

--- client/src/js/client.js
+++ client/src/js/client.js
@@ -8,6 +8,7 @@ var Snow = require('./snow.js');
 var snow = new Snow(dom, tool);
 var time = 0;
 var count = 0;
+var trackNumber = 0;
 
 //On window resize
 $(window).resize(function() {
@@ -31,7 +32,7 @@ $(document).keydown(function(e) {
 //Complete logic of the cycle of the app goes here
 var cycle = {};
 cycle.start = function(data) {
-    playSong(data);
+    playSong(data, trackNumber);
     cycle.loop(data);
 };
 cycle.loop = function(data){
@@ -46,12 +47,7 @@ cycle.loop = function(data){
 
     //Counts the amount of times there is no difference between playing time
     count = (audio.deltaTime(time) ?  0 : count + 1);
-
-    if(/*!audio.isPlaying(count)*/ time > 5) {
-      count = 0;
-      audio.pause();
-      playSong(data);
-    }
+    checkSongFinished(data);
 
     //Declare at the end for delay
     time = audio.getTime();
@@ -63,9 +59,27 @@ tool.getTracklist(function(data) {
 });
 
 //Play song
-playSong = function(data) {
-  var randomTrack = data[Math.floor(Math.random() * data.length)];
-  dom.setTrackInfo(randomTrack.ytTitle, randomTrack.name);
-  dom.changeTheme(randomTrack.genre.split('.')[0]-1);
-  audio.play(randomTrack.src);
+playSong = function(data, i) {
+  var track = data[i];
+  dom.setTrackInfo(track.ytTitle, track.name);
+  dom.changeTheme(track.genre.split('.')[0]-1);
+  audio.play(track.src);
+};
+
+checkSongFinished = function(data) {
+  if(!audio.isPlaying(count)) {
+    count = 0;
+    trackNumber++;
+    audio.pause();
+
+    if (trackNumber > data.length - 1) {
+      songsFinished();
+    } else {
+      playSong(data, trackNumber);
+    }
+  }
+};
+
+songsFinished = function(){
+  console.log('klaar');
 };
