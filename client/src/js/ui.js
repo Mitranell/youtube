171d409e743e0b6fd23e75ce079e44d68e1682e5
Mitranell <milantheeuwes@live.nl>
2015-12-21 20:38:07 +0100

    Rotation upgraded
    Better scripted to manipulate variables:
    - range
    - degree

--- client/src/js/ui.js
+++ client/src/js/ui.js
@@ -12,9 +12,12 @@ var ui = function(dom) {
         c.c.height = dom.canvasWrapperHeight();
     };
     this.render = function(spectrumData) {
+        var x = 0;
         var max = 0;
         var rotation = 0;
-        var x = 0;
+        var range = 10; //Range of bars who determine the rotation, bars above range is all full to the right
+        var degrees = 5; //Ammount of degrees the skull is rotate left and right
+
         var spectrum = {};
         spectrum.data = spectrumData;
         spectrum.size = spectrum.data.length / 6; //Only display the first 1/6th of the spectrum
@@ -22,15 +25,18 @@ var ui = function(dom) {
 
         //Draw frequencyBars to canvas
         c.ctx.clearRect(0, 0, dom.canvasWrapperWidth(), dom.canvasWrapperHeight());
-        for (var i = 0; i < spectrum.size; i++) {
-            spectrum.barHeight = spectrum.data[i] * dom.canvasWrapperHeight();
-            c.ctx.fillStyle = color.white;
-            c.ctx.fillRect(x, dom.canvasWrapperHeight() / 2 - spectrum.barHeight / 2, spectrum.barWidth, spectrum.barHeight);
-            x += spectrum.barWidth * 2; //Makes it display 1/12th of the spectrum
+        for (var i = 0; i < spectrum.data.length; i++) {
+            if(i < spectrum.size){
+              spectrum.barHeight = spectrum.data[i] * dom.canvasWrapperHeight();
+              c.ctx.fillStyle = color.white;
+              c.ctx.fillRect(x, dom.canvasWrapperHeight() / 2 - spectrum.barHeight / 2, spectrum.barWidth, spectrum.barHeight);
+              x += spectrum.barWidth * 2; //Makes it display 1/12th of the spectrum
+            }
 
             if (max < spectrum.data[i]) {
               max = spectrum.data[i];
-              rotation = (2 * i / spectrum.size - 1) * 10; //Rotate left and right 10 degrees
+              bar = (i > range) ? range : i;
+              rotation = (2 * bar / range  - 1) * degrees;
             }
         }
 
